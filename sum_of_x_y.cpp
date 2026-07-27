9a93bde372b2bd22dda6d7d30ade860fb1ba2614
Sahil Shinde <sahilshinde352007@gmail.com>
2026-07-27 22:05:11 +0530

    Day 2: Sum of Two Numbers using Input and Output

---
+++ sum_of_x_y.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+
+    cout << "Enter x: ";
+    cin >> x;
+
+    cout << "Enter y: ";
+    cin >> y;
+
+    cout << "Sum of x and y is: " << x + y << endl;
+
+    return 0;
+}
