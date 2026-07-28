8cf48a0413783d8d195df88e3748872b018e2975
Sahil Shinde <sahilshinde352007@gmail.com>
2026-07-28 18:50:05 +0530

    Day 3: Area of Rectangle

---
+++ area_of_rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+main(){
+    int l;
+    int b;
+    cout << "Enter Length: ";
+    cin >> l;
+    cout << "Enter Width: ";
+    cin >> b;
+    cout <<"Area of rectangle is: ";
+    cout << l*b;
+}
