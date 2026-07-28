b396c71d16f9774eddbfe182ca8d8114dfaa3614
Sahil Shinde <sahilshinde352007@gmail.com>
2026-07-28 18:49:35 +0530

    Day 3: Area of Circle

---
+++ area_of_circle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main(){
+    float radius;
+    cout << "Enter Radius Of Circle: ";
+    cin >> radius;
+    cout << "Area Of Circle Is: ";
+    cout << 3.14 * radius * radius ;
+}
