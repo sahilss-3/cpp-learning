11395e4ce440fbd4877f75c57e52166dcdd006c5
Sahil Shinde <sahilshinde352007@gmail.com>
2026-08-08 23:53:18 +0530

    Day 14: Factorial of number

---
+++ factorial_of_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int factorial = 1;
+
+    for (int i = 1; i <= n; i++) {
+        factorial = factorial * i;
+    }
+
+    cout << "Factorial = " << factorial;
+
+    return 0;
+}
